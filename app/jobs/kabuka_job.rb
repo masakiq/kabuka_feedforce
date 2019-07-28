65d486549d2e38fe877476e407d798d4634dc398
maeda1150 <masaki19770108@gmail.com>
2019-07-29 01:39:42 +0900

    first commit
    new file:   .gitignore
    new file:   .jetskeep
    new file:   Gemfile
    new file:   Gemfile.lock
    new file:   README.md
    new file:   Rakefile
    new file:   app/jobs/application_job.rb
    new file:   app/jobs/kabuka_job.rb
    new file:   config/application.rb
    new file:   config/environments/development.rb
    new file:   config/environments/production.rb
    new file:   config/environments/test.rb

---
+++ app/jobs/kabuka_job.rb
@@ -0,0 +1,41 @@
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require 'uri'
+require 'json'
+
+class KabukaJob < ApplicationJob
+  class_timeout 300 # 300s or 5m, current Lambda max is 15m
+
+  rate "10 hours" # every 10 hours
+  def post
+    post_slack get_kabuka
+  end
+
+  private
+
+  def get_kabuka
+    url = 'https://stocks.finance.yahoo.co.jp/stocks/detail/?code=7068'
+    charset = nil
+
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
+
+    doc = Nokogiri::HTML.parse(html, nil, charset)
+    doc.css('.stoksPrice').text.strip
+  end
+
+  def post_slack(kabuka)
+    uri = URI.parse(ENV['SLACK_WEBHOOK_URL'])
+    params = { text: "フィードフォース株価 : #{ kabuka.to_s }" }
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.start do
+      request = Net::HTTP::Post.new(uri.path)
+      request.set_form_data(payload: params.to_json)
+      http.request(request)
+    end
+  end
+end
